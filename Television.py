2a03d71d90c39c1b2ddc080b5ae79f98a257fdce
angelinetipa <angelinetipa@gmail.com>
2023-05-16 23:32:46 +0800

    constructor chhanel, volumelev, on

--- Television.py
+++ Television.py
@@ -1,6 +1,11 @@
 # class named TV
 class TV:
-# constructor channel, volume_level, on
+    # constructor channel, volume_level, on
+    def __init__(tv, channel, volume_level, on):
+        tv.channel = channel
+        tv.volume_level = volume_level
+        tv.on = on
+
 # method to turn om
 # method to turn off
 # method to get channel
