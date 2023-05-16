2265e209b4bf03b0577319308c0b82ece067a04c
angelinetipa <angelinetipa@gmail.com>
2023-05-16 23:25:01 +0800

    pseudocode

---
+++ Television.py
@@ -0,0 +1,13 @@
+# class named TV
+# constructor channel, volume_level, on
+# method to turn om
+# method to turn off
+# method to get channel
+# method to set a new channel
+# method to get volume
+# method to set a new volume
+# method to channel up
+# method to channel down
+# method to volume up
+# method to volumw down
+# call function
