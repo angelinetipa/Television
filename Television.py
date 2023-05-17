30247b325c7b01d3c13f4a74ac0c1cc21134b6ba
angelinetipa <angelinetipa@gmail.com>
2023-05-17 16:03:17 +0800

    add/change returns

--- Television.py
+++ Television.py
@@ -21,25 +21,31 @@ class TV:
         return "The TV is off!"
     # method to get channel
     def get_channel(tv):
-        print(f"The channel for this TV is {tv.channel}")
+        return f"TV's current channel:  {tv.channel}"
     # method to set a new channel
     def set_channel(tv, new_channel):
         tv.channel = new_channel
+        return f"TV's new channel:  {tv.channel}"
     # method to get volume
     def get_volume(tv):
-        print(f"The volume for this TV is {tv.volume_level}")
+        return f"TV'S current volume:  {tv.volume_level}"
     # method to set a new volume
     def set_volume(tv, new_volume):
         tv.volume_level = new_volume
+        return f"TV's new volume:  {tv.volume_level}"
     # method to channel up
     def channel_up(tv, channel_up):
         tv.channel += channel_up
+        return f"TV's new channel:  {tv.channel}"
     # method to channel down
     def channel_down(tv, channel_down):
         tv.channel -= channel_down
+        return f"TV's new channel:  {tv.channel}"
     # method to volume up
     def volume_up(tv, volume_up):
         tv.volume_level += volume_up
+        return f"TV's new volume:  {tv.volume_level}"
     # method to volume down
     def volume_down(tv, volume_down):
         tv.volume_level += volume_down
+        return f"TV's new volume:  {tv.volume_level}"
