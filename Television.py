634d8967715703e05f42fb3c711455f9ce6ff95d
angelinetipa <angelinetipa@gmail.com>
2023-05-17 21:32:44 +0800

    did several changes

--- Television.py
+++ Television.py
@@ -3,31 +3,22 @@ class TV:
     # constructor channel, volume_level, on
     def __init__(tv, channel, volume_level, on):
         tv.channel = channel
-        # raise ValueError
-        if channel <= 0 and channel >= 121:
-            raise ValueError(f"channel expected only 1 to 120, got {channel}")
         tv.volume_level = volume_level
-        if volume_level <= 0 and volume_level >= 8:
-            raise ValueError(f"channel expected only 1 to 7, got {volume_level}")
         tv.on = on
 
     # method to turn on
     def turn_on(tv):
-        if tv.on == True:
-            return f"TV is already on!"
-        else:
-            return f"TV is on!"
+        tv.on = True
+        return f"TV is on!"
     # method to turn off
     def turn_off(tv):
-        if tv.on == False:
-            return f"TV is already off!"
-        else:
-            return f"TV is off!"
+        tv.on = False
+        return f"TV is off!"
     # method to get channel
     def get_channel(tv):
         return f"TV's current channel:  {tv.channel}"
     # method to set a new channel
-    def set_channel(tv, new_channel):
+    def set_channel(tv, new_channel): 
         tv.channel = new_channel
         return f"TV's new channel:  {tv.channel}"
     # method to get volume
@@ -40,16 +31,16 @@ class TV:
     # method to channel up
     def channel_up(tv, channel_up):
         tv.channel += channel_up
-        return f"TV's new channel:  {tv.channel}"
+        return f"TV's new channel(up):  {tv.channel}"
     # method to channel down
     def channel_down(tv, channel_down):
         tv.channel -= channel_down
-        return f"TV's new channel:  {tv.channel}"
+        return f"TV's new channel(down):  {tv.channel}"
     # method to volume up
     def volume_up(tv, volume_up):
         tv.volume_level += volume_up
-        return f"TV's new volume:  {tv.volume_level}"
+        return f"TV's new volume(up):  {tv.volume_level}"
     # method to volume down
     def volume_down(tv, volume_down):
-        tv.volume_level += volume_down
-        return f"TV's new volume:  {tv.volume_level}"
+        tv.volume_level -= volume_down
+        return f"TV's new volume(down):  {tv.volume_level}"
--- TestTV.py
+++ TestTV.py
@@ -1,12 +1,20 @@
 # import television
 from Television import TV
-# def main function
-def main():
-    # create tv1 
-    tv1 = TV(30, 3, False)
-    print(f"\ntv1's channel is {tv1.channel} and volume level is {tv1.volume_level}")
-    # create tv2
-    tv2 = TV(3, 2, True)
-    print(f"\ntv2's channel is {tv2.channel} and volume level is {tv2.volume_level}\n")
-# call main function
-main()
+
+# create tv1 
+tv1 = TV(30, 3, False)
+print(f"\ntv1's channel is {tv1.channel} and volume level is {tv1.volume_level}")
+ # create tv2
+tv2 = TV(3, 2, True)
+print(f"\ntv2's channel is {tv2.channel} and volume level is {tv2.volume_level}\n")
+
+# calling methods
+print(tv1.turn_on())
+print(tv1.get_channel())
+print(tv1.set_channel(50))
+print(tv1.get_volume())
+print(tv1.set_volume(6))
+print(tv1.channel_up(3))
+print(tv1.channel_down(1))
+print(tv1.volume_up(1))
+print(tv1.volume_down(3))
